a6cc67dba4e8a29193e7ee99b4a3dfa99e5f055c
Semir Selman <selmansemir555@gmail.com>
2021-10-29 08:09:06 +0200

    make functionality for the main header

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,3 +1,29 @@
-exports.main = (req, res) => {
-    res.render('home');
+const db = require('../config/dataBase');
+
+module.exports = {
+    main: async function(req, res) {
+
+        const con = db.getCon();
+
+        let data = await con.promise().query('SELECT content_id, header, article, img, content_time FROM content');
+        
+        console.log(data[0]);
+
+        res.render('main/home.ejs', {data: data[0]});
+    },
+    hardware: function (req, res) {
+        res.render('main/hardware.ejs');
+    },
+    software: function(req, res) {
+        res.render('main/software.ejs');
+    },
+    network:function(req, res) {
+        res.render('main/network.ejs');
+    },
+    code: function(req, res) {
+        res.render('main/code.ejs');
+    },
+    aboutUs: function(req, res) {
+        res.render('main/about.ejs');
+    }
 }
---
+++ src/public/js/mainHeader.js
@@ -0,0 +1,48 @@
+let Nav_Other_List = document.getElementById('nav-other');
+let darkBack = document.getElementById('dark-backgrund');
+
+document.addEventListener('scroll', () => {
+
+    darkBack.style.display = "none";
+    Nav_Other_List.style.display = "none";
+    
+});
+
+function clickDarkBack() {
+
+    darkBack.style.display = "none";
+    Nav_Other_List.style.display = "none";
+}
+
+function dropDownList() {
+
+    Nav_Other_List.style.display = "block";
+    darkBack.style.display = "block";
+
+}
+
+function hardware() {
+
+}
+
+function software() {
+
+    
+
+}
+
+function network() {
+
+    
+
+}
+
+function code() {
+
+    
+
+}
+
+function aboutUs() {
+    
+}
